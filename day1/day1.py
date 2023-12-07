08c9547ba08a73c404f9ee3869bfdc3d4febfe71
Rcatarata <Rcatarata@gmail.com>
2023-12-06 20:01:16 -0500

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,5 @@
+# printing and using input for questions on a script
+print('Welcome to name generator')
+question1 = input('what is your name?')
+question2 = input('what is your last name?')
+print('Your new name is ' + question1 + 'tron ' + question2 + 'ye')
